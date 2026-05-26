90423cea8bfeb2faf473a545eede9ccd2083edc5
pitipiwpiwwiw <oikawaa012345@gmail.com>
2026-05-26 09:19:44 +0800

    Create huadgu.cpp

---
+++ huadgu.cpp
@@ -0,0 +1,40 @@
+body {
+    front-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    background-color: #0000FF;
+
+}
+
+header {
+    background: #4CAF50;
+    color: white;
+    padding: 20px;
+}
+
+main{
+    margin: 40px;
+}
+
+button {
+    padding: 10px 20px;
+    font-size: 50px;
+    background: 4CAF50;
+    color: white;
+    border: none;
+    cursor: pointer;
+}
+
+button:hover {
+    background: #45a049;
+}
+
+footer {
+    background: #333;
+    color: white;
+    padding: 10px;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
